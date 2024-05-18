b053a3c5a63c67ead8e626b55ae34f01c181b827
Shamim Sheikh <shamimsheikh@Shamims-MacBook.local>
2024-05-18 12:40:18 +0530

    Our blog home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Mission from '@/components/mission/Mission';
 import Achievements from '@/components/acievements/Achievements';
 import AboutAccordion from '@/components/aboutAccordion/AboutAccordion';
 import Bhag from '@/components/bhag/Bhag';
-import BlogList from '@/components/blogList/BlogList';
+import BlogList from '@/components/bloglist/BlogList';
 export default function Home() {
   return (
     <main className="">
