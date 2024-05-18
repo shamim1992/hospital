0df58b66d1dbfc6175fb520de8732db872676f44
Shamim Sheikh <shamimsheikh@Shamims-MacBook.local>
2024-05-18 10:43:59 +0530

    Our Achievements

--- src/components/bhag/Bhag.js
+++ src/components/bhag/Bhag.js
@@ -5,7 +5,7 @@ import bhagImage from '../../assets/bhag.jpeg'
 const Bhag = () => {
   return (
     <div className='my-10 bg-[#1C75BC]'>
-{/* 
+
         <div className='flex flex-col lg:flex-row justify-center items-center'>
             <div className=''>
                 <Image src={bhagImage} alt='image' className='w-full lg:h-[620px]' />
@@ -14,7 +14,7 @@ const Bhag = () => {
                 <h1 className='text-3xl font-bold py-10'>Big Highly Audacious Goal (BHAG)</h1>
                 <p className='max-w-xl'>Ultimately, as a think-tank and thought-leadership organization, FAED aims to provide the policy and regulatory framework for a strong and functional evidence based laboratory medicine system in south-east Asia to maintain and sustain the quality of high value healthcare delivery following CLIA regulations and ISO medical laboratory standards.</p>
             </div>
-        </div> */}
+        </div>
 
     </div>
   )
